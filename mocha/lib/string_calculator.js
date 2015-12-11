6c749f91e2c74d69e8594c393a77f66d96b3985b
Bojan Čoka <metropolis.lights@gmail.com>
2015-12-11 19:30:58 +0100

    Make use of Automatic Semicolon Insertion

--- mocha/lib/string_calculator.js
+++ mocha/lib/string_calculator.js
@@ -1,23 +1,23 @@
-"use strict";
+"use strict"
 
 module.exports = function StringCalculator() {
 
   function add(input) {
 
     if (input === "") {
-      return 0;
+      return 0
     }
 
     function toInteger(currentValue, index, array) {
-      return parseInt(currentValue, 10);
+      return parseInt(currentValue, 10)
     }
 
     function sum(previousValue, currentValue, currentIndex, array) {
-      return previousValue + currentValue;
+      return previousValue + currentValue
     }
 
-    return input.split(",").map(toInteger).reduce(sum);
+    return input.split(",").map(toInteger).reduce(sum)
   }
 
-  return { add : add };
-}();
+  return { add : add }
+}()
--- mocha/test/string_calculator_test.js
+++ mocha/test/string_calculator_test.js
@@ -1,7 +1,7 @@
-"use strict";
+"use strict"
 
-var assert = require("assert");
-var StringCalculator = require("../lib/string_calculator.js");
+var assert = require("assert")
+var StringCalculator = require("../lib/string_calculator.js")
 
 describe("StringCalculator", function() {
 
@@ -10,42 +10,42 @@ describe("StringCalculator", function() {
     context("given an empty string", function() {
 
       it("returns zero", function() {
-        assert.equal(0, StringCalculator.add(""));
-      });
-    });
+        assert.equal(0, StringCalculator.add(""))
+      })
+    })
 
     context("single numbers", function() {
 
       context("given '4'", function() {
 
         it("returns 4", function() {
-          assert.equal(4, StringCalculator.add("4"));
-        });
-      });
+          assert.equal(4, StringCalculator.add("4"))
+        })
+      })
 
       context("given '10'", function() {
 
         it("returns 10", function() {
-          assert.equal(10, StringCalculator.add("10"));
-        });
-      });
-    });
+          assert.equal(10, StringCalculator.add("10"))
+        })
+      })
+    })
 
     context("comma-separated numbers", function() {
 
       context("given '2,4'", function() {
 
         it("returns 6", function() {
-          assert.equal(6, StringCalculator.add("2,4"));
-        });
-      });
+          assert.equal(6, StringCalculator.add("2,4"))
+        })
+      })
 
       context("given '17,100'", function() {
 
         it("returns 117", function() {
-          assert.equal(117, StringCalculator.add("17,100"));
-        });
-      });
-    });
-  });
-});
+          assert.equal(117, StringCalculator.add("17,100"))
+        })
+      })
+    })
+  })
+})
