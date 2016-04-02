6698be8a31408439bf8207325be91200bde76331
Bojan Čoka <metropolis.lights@gmail.com>
2016-04-02 19:49:17 +0200

    [clojure/hobbit-crusher] Clojure 1.7.0 -> 1.8.0

--- clojure/hobbit-crusher/project.clj
+++ clojure/hobbit-crusher/project.clj
@@ -1,5 +1,5 @@
 (defproject hobbit-crusher "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]]
   :main ^:skip-aot hobbit-crusher.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
